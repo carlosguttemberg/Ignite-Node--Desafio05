35ca126c35787bb30be64c0eb6a727c3110a2e87
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-07-02 20:02:51 -0300

    first commit

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,32 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    // Complete usando ORM
+    const user = await this.repository.findOneOrFail(user_id, {relations: ["games"]});
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query("select * from users order by first_name"); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query("select * from users where first_name = '" + first_name + "' and last_name = '" + last_name + "'"); // Complete usando raw query
+  }
+}
